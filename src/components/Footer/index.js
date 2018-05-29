94ceff7d3d6bf797dfe76690497faa3613d00552
Georgios Andreadis <G.Andreadis@student.tudelft.nl>
2018-05-29 13:25:46 +0200

    Fix Header component name in Footer/index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Header = () => (
+const Footer = () => (
   <div className="bg-blue">
     <div className="flex justify-between max-w-xl mx-auto p-4 md:p-8 text-sm">
       <p className="text-white">
@@ -24,4 +24,4 @@ const Header = () => (
   </div>
 );
 
-export default Header;
+export default Footer;
