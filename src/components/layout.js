d88db0babd8c34fdf6082e456b5bc9afcab0c137
Luke Bennett <hello@lukebennett.com.au>
2019-07-30 20:38:47 +1000

    Switch to useStaticQuery

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,54 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { StaticQuery, graphql } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 
 import Header from "./header";
 
 function Layout({ children }) {
-  return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
-            }
-          }
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
         }
-      `}
-      render={data => (
-        <div className="flex flex-col font-sans min-h-screen text-gray-900">
-          <Header siteTitle={data.site.siteMetadata.title} />
+      }
+    }
+  `);
 
-          <main className="flex flex-col flex-1 md:justify-center max-w-4xl mx-auto px-4 py-8 md:p-8 w-full">
-            {children}
-          </main>
+  return (
+    <div className="flex flex-col font-sans min-h-screen text-gray-900">
+      <Header siteTitle={data.site.siteMetadata.title} />
 
-          <footer className="bg-blue-700">
-            <nav className="flex justify-between max-w-4xl mx-auto p-4 md:p-8 text-sm">
-              <p className="text-white">
-                Created by{" "}
-                <a
-                  href="https://taylorbryant.blog"
-                  className="font-bold no-underline text-white"
-                >
-                  Taylor Bryant
-                </a>
-              </p>
+      <main className="flex flex-col flex-1 md:justify-center max-w-4xl mx-auto px-4 py-8 md:p-8 w-full">
+        {children}
+      </main>
 
-              <p>
-                <a
-                  href="https://github.com/taylorbryant/gatsby-starter-tailwind"
-                  className="font-bold no-underline text-white"
-                >
-                  GitHub
-                </a>
-              </p>
-            </nav>
-          </footer>
-        </div>
-      )}
-    />
+      <footer className="bg-blue-700">
+        <nav className="flex justify-between max-w-4xl mx-auto p-4 md:p-8 text-sm">
+          <p className="text-white">
+            Created by{" "}
+            <a
+              href="https://taylorbryant.blog"
+              className="font-bold no-underline text-white"
+            >
+              Taylor Bryant
+            </a>
+          </p>
+
+          <p>
+            <a
+              href="https://github.com/taylorbryant/gatsby-starter-tailwind"
+              className="font-bold no-underline text-white"
+            >
+              GitHub
+            </a>
+          </p>
+        </nav>
+      </footer>
+    </div>
   );
 }
 
