ed7fba1b636853fc35a28b1d7398d72b10e0b834
Taylor Bryant <taylor@bryant.io>
2019-09-29 00:10:19 -0500

    Update URL and remove unnecessary static query

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,12 @@
-import React from "react";
 import PropTypes from "prop-types";
-import { graphql, useStaticQuery } from "gatsby";
+import React from "react";
 
 import Header from "./header";
 
 function Layout({ children }) {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
   return (
     <div className="flex flex-col font-sans min-h-screen text-gray-900">
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header />
 
       <main className="flex flex-col flex-1 md:justify-center max-w-4xl mx-auto px-4 py-8 md:p-8 w-full">
         {children}
@@ -26,9 +15,9 @@ function Layout({ children }) {
       <footer className="bg-blue-700">
         <nav className="flex justify-between max-w-4xl mx-auto p-4 md:p-8 text-sm">
           <p className="text-white">
-            Created by{" "}
+            Created by{` `}
             <a
-              href="https://taylorbryant.blog"
+              href="https://bryant.io"
               className="font-bold no-underline text-white"
             >
               Taylor Bryant
