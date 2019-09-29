c84e143a0949064bc7ea284d2d9960b19734f05b
Taylor Bryant <taylor@bryant.io>
2019-09-29 00:28:36 -0500

    Minify CSS using cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
 const tailwindcss = require(`tailwindcss`);
 
 module.exports = {
-  plugins: [tailwindcss(`./tailwind.config.js`), require(`autoprefixer`)]
+  plugins: [
+    tailwindcss(`./tailwind.config.js`),
+    require(`autoprefixer`),
+    require(`cssnano`)({
+      preset: `default`
+    })
+  ]
 };
