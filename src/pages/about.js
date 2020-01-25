77e72f0ca8ff2e43c947d311175a7068d9cb9a4a
Taylor Bryant <taylor@bryant.io>
2020-01-25 10:59:48 -0600

    Use escaped quote

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,17 +12,17 @@ function AboutPage() {
         title="About"
       />
 
-      <section className="flex flex-col md:flex-row items-center">
+      <section className="flex flex-col items-center md:flex-row">
         <div className="md:w-2/3 md:mr-8">
-          <blockquote className="border-l-4 border-gray-900 font-serif leading-loose pl-4 text-justify">
-            The point is... to live one's life in the full complexity of what
-            one is, which is something much darker, more contradictory, more of
-            a maelstrom of impulses and passions, of cruelty, ecstacy, and
-            madness, than is apparent to the civilized being who glides on the
-            surface and fits smoothly into the world.
+          <blockquote className="pl-4 font-serif leading-loose text-justify border-l-4 border-gray-900">
+            The point is... to live one&apos;s life in the full complexity of
+            what one is, which is something much darker, more contradictory,
+            more of a maelstrom of impulses and passions, of cruelty, ecstacy,
+            and madness, than is apparent to the civilized being who glides on
+            the surface and fits smoothly into the world.
           </blockquote>
 
-          <cite className="font-bold mt-4 text-right text-xs uppercase block">
+          <cite className="block mt-4 text-xs font-bold text-right uppercase">
             – Thomas Nagel
           </cite>
         </div>
