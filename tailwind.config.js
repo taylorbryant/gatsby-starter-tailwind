0bf5307b8ed338c590688eecfb236391853dae01
Luke Bennett <hello@lukebennett.com.au>
2020-04-30 12:26:40 +1000

    Use PurgeCSS built into Tailwind 1.4 (#571)
    * Use PurgeCSS built into Tailwind 1.4
    
    * Pin release of tailwindcss to 1.4.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
+  purge: [
+    './src/**/*.js',
+  ],
   theme: {},
   variants: {},
   // https://github.com/tailwindcss/custom-forms
