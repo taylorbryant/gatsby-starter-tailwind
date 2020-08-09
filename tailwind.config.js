357c6314974e730a0c7080df6b27d2537b09b626
Taylor Bryant <taylor@bryant.io>
2020-08-09 10:15:58 -0500

    Run Prettier over codebase

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
-  purge: [
-    './src/**/*.js',
-  ],
+  purge: ["./src/**/*.js"],
   theme: {},
   variants: {},
   // https://github.com/tailwindcss/custom-forms
